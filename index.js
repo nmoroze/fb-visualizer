1fa669e15c4dd70ff1f449d09e3bb578d55d61e1
Noah Moroze <noahmoroze@gmail.com>
2017-04-29 18:17:31 -0400

    Fixed some frontend stuff for Alan

--- index.js
+++ index.js
@@ -55,21 +55,11 @@ var success = function (data) {
 
 var twitter = new Twitter(config);
 
-//Example calls
+//var parameters = {
+//
+//}; 
+////Example calls
+//twitter.getFollowersList(parameters, errorCallback, successCallback);
 
-twitter.getUserTimeline({ screen_name: 'BoyCook', count: '10'}, error, success);
-twitter.getMentionsTimeline({ count: '10'}, error, success);
-twitter.getHomeTimeline({ count: '10'}, error, success);
-twitter.getReTweetsOfMe({ count: '10'}, error, success);
-twitter.getTweet({ id: '1111111111'}, error, success);
 
-//
-// Get 10 tweets containing the hashtag haiku
-//
-twitter.getSearch({'q':'#haiku','count': 10}, error, success);
-
-//
-// Get 10 popular tweets with a positive attitude about a movie that is not scary 
-//
-twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success);
 
